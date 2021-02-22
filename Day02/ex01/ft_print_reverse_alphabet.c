b65fda9189c768a7439332533f02626097798f1b
Simon Robert <simonrobert974@gmail.com>
2021-02-22 15:18:33 +0100

    ex01 Day2 done

---
+++ Day02/ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+void    ft_print_reverse_alphabet(void);
+
+int     main(void){
+    ft_print_reverse_alphabet();
+    return (0);
+}
+
+void    ft_print_reverse_alphabet(void){
+    write(1,"zyxwvutsrqponmlkjihgfedcba",26);
+}
