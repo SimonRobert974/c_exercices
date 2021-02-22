040aa7aa4fbd7f6de30e6df2b72590e6b43b9e18
Simon Robert <simonrobert974@gmail.com>
2021-02-22 16:01:53 +0100

    fixed version

--- Day02/ex02/ft_print_numbers.c
+++ Day02/ex02/ft_print_numbers.c
@@ -1,9 +1,22 @@
 #include <unistd.h>
 void    ft_print_numbers(void);
+void    ft_putchar(char c);
+
 int     main(void){
     ft_print_numbers();
     return(0);
 }
+
 void    ft_print_numbers(void){
-    write(1,"0123456789",10);
+    int i;
+
+    i = 0;
+    while(i < 10){
+        ft_putchar('0' + i);
+        i++;
+    }
+}
+
+void ft_putchar(char c){
+    write(1, &c, 1);
 }
